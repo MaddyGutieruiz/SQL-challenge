bc0b7f60ffafeed30b6c7fd00d79a55c200c6fa8
MaddyGutieruiz <119770462+MaddyGutieruiz@users.noreply.github.com>
2023-02-24 16:22:10 -0800

    Add files via upload

---
+++ SQL_Data_Analysis_Code.sql
@@ -0,0 +1,59 @@
+-- Q1: List the employee number, last name, first name, sex, and salary of each employee
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees AS e 
+LEFT JOIN salaries AS s 
+ON e.emp_no = s.emp_no
+ORDER BY e.emp_no 
+
+--Q2: List the first name, last name, and hire date for the employees who were hired in 1986 
+SELECT e.first_name, e.last_name, e.hire_date 
+FROM employees AS e 
+WHERE e.hire_date BETWEEN '1986-01-01' AND '1986-12-31'
+
+--Q3: List the manager of each department along with their department number, department name, employee number, last name, and first name
+SELECT d.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+FROM dept_manager AS dm
+INNER JOIN departments AS d
+ON dm.dept_no = d.dept_no 
+INNER JOIN employees AS e 
+ON dm.emp_no = e.emp_no 
+
+--Q4: List the department number for each employee along with that employee’s employee number, last name, first name, and department name
+SELECT dm.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+FROM dept_manager AS dm
+JOIN departments AS d
+ON dm.dept_no = d.dept_no
+JOIN employees AS e
+ON dm.emp_no = e.emp_no;
+
+--Q5: List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B
+SELECT e.first_name, e.last_name, e.sex
+FROM employees AS e
+WHERE e.first_name = 'Hercules'
+AND e.last_name LIKE 'B%';
+
+--Q6: List each employee in the Sales department, including their employee number, last name, and first name
+SELECT e.emp_no, e.last_name, e.first_name
+FROM employees AS e
+JOIN dept_emp AS de
+ON de.emp_no = e.emp_no
+JOIN departments AS d 
+ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales'
+ORDER BY e.emp_no;
+
+--Q7: List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+JOIN dept_emp AS de
+ON de.emp_no = e.emp_no
+JOIN departments AS d 
+ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development')
+ORDER BY e.emp_no;
+
+--Q8: List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name)
+SELECT e.last_name, COUNT(e.last_name) AS "last_name_count"
+FROM employees AS e
+GROUP BY e.last_name
+ORDER BY "last_name_count" DESC;
